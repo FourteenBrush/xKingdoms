051ddf4eb9a870ac477ff58a9a158101ce47ecdc
FourteenBrush <naessensarthur2@protonmail.com>
2022-05-21 23:37:22 +0200

    A StorageType now has its own DataSource

--- src/main/java/me/fourteendoggo/xkingdoms/storage/ConnectionFactory.java
+++ src/main/java/me/fourteendoggo/xkingdoms/storage/ConnectionFactory.java
@@ -1,15 +1,32 @@
 package me.fourteendoggo.xkingdoms.storage;
 
+import com.zaxxer.hikari.HikariDataSource;
 import me.fourteendoggo.xkingdoms.Xkingdoms;
 
-import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ConnectionFactory {
-    private final DataSource dataSource = null;
+    private final HikariDataSource dataSource;
     private final StorageType type;
 
     public ConnectionFactory(Xkingdoms plugin) {
-        String typeString = plugin.getConfig().getString("database.type");
-        this.type = StorageType.fromString(typeString, StorageType.H2);
+        String type = plugin.getConfig().getString("database.type");
+        this.type = StorageType.fromString(type, StorageType.H2);
+        this.dataSource = this.type.getDataSource(plugin);
+    }
+
+    public Connection getConnection() throws SQLException {
+        return this.dataSource.getConnection();
+    }
+
+    public StorageType getType() {
+        return this.type;
+    }
+
+    public void close() {
+        if (this.dataSource != null && !this.dataSource.isClosed()) {
+            this.dataSource.close();
+        }
     }
 }
